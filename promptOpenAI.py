605a164af6d2e5680aa4090f10119b2a3827cd01
Jeannie Teo <jeannieteo78@gmail.com>
2025-06-12 09:19:11 +0800

    Update promptOpenAI.py

--- promptOpenAI.py
+++ promptOpenAI.py
@@ -79,3 +79,25 @@ print(response)
 
 #-----Tactic 4: "Few-shot" prompting
 # give few shot example for good answer
+
+
+#-----
+user_messages = [
+  "La performance du système est plus lente que d'habitude.",  # System performance is slower than normal         
+  "Mi monitor tiene píxeles que no se iluminan.",              # My monitor has pixels that are not lighting
+  "Il mio mouse non funziona",                                 # My mouse is not working
+  "Mój klawisz Ctrl jest zepsuty",                             # My keyboard has a broken control key
+  "我的屏幕在闪烁"                                               # My screen is flashing
+] 
+
+for issue in user_messages:
+    prompt = f"Tell me what language this is: ```{issue}```"
+    lang = get_completion(prompt)
+    print(f"Original message ({lang}): {issue}")
+
+    prompt = f"""
+    Translate the following  text to English \
+    and Korean: ```{issue}```
+    """
+    response = get_completion(prompt)
+    print(response, "\n")
